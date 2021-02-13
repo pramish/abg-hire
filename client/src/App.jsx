29c43ed3b0f787aa8b0d090f40a8a8d0a5da3ea3
Pramish <pramish.luitel7@gmail.com>
2021-02-13 23:31:11 +1100

    add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
+import { Provider } from "react-redux";
+import configureStore from "./store";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
-import { Provider } from "react-redux";
-import configureStore from "./store";
 import ActivateAccount from "./components/activate_account/activate_account";
+import Navbar from "./components/navbar/navbar";
+import Auth from "./components/auth/auth";
 
 function App() {
   return (
     <Provider store={configureStore}>
       <Router>
+        <Navbar />
         <Route path="/activate-account" component={ActivateAccount} />
-        <Login />
-        <Register />
+        <Route exact path="/accounts" component={Auth} />
       </Router>
     </Provider>
   );
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/abg-hire-logo.png";
+import { Link } from "react-router-dom";
 
-export default function navbar() {
-    return (
-        <div>
-            
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="headerMenu">
+        <div className="logoHeader">
+          <Link to="/">
+            <img className="logoImage" alt="Header Logo" src={Logo} />
+          </Link>
         </div>
-    )
+        <div className="menuItems">
+          <ul>
+            <Link to="/accounts">
+              <li>Authentication</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
